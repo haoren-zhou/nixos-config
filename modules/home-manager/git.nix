e7ebb89ae5954926b7bcbad871df8ca73e0adaf9
haoren-zhou <hrz9009@gmail.com>
2025-07-12 02:28:36 +0800

    Initial nixos config commit

---
+++ modules/home-manager/git.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "haoren-zhou";
+    userEmail = "hrz9009@gmail.com";
+  };
+}
---
+++ modules/home-manager/micro.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  programs.micro = {
+    enable = true;
+    settings = {
+      autosu = true;
+      tabsize = 2;
+      tabstospaces = true;
+      softwrap = true;
+    };
+  };
+}
