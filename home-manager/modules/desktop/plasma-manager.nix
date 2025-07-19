2c52543d4b32a1e84e7c0e22ed8bd3634e254784
Hao Ren Zhou <hrz9009@gmail.com>
2025-07-19 23:13:36 +0800

    Update plasma-manager.nix

--- home-manager/modules/desktop/plasma-manager.nix
+++ home-manager/modules/desktop/plasma-manager.nix
@@ -14,6 +14,8 @@
     # overrideConfig = true;
 
     workspace = {
+      enableMiddleClickPaste = false;
+      clickItemTo = "select";
       cursor = {
         theme = "Bibata-Modern-Ice";
         size = 24;
@@ -40,6 +42,57 @@
         name = "Capture the entire desktop";
         key = "Meta+Ctrl+S";
         command = "spectacle --fullscreen --nonotify";
+      };
+    };
+
+    shortcuts = {
+      ksmserver = {
+        "Lock Session" = [
+          "Screensaver"
+          "Meta+L"
+        ];
+        "LogOut" = [
+          "Meta+Shift+L"
+        ];
+      };
+
+      "KDE Keyboard Layout Switcher" = {
+        "Switch to Next Keyboard Layout" = "Meta+Space";
+      };
+
+      kwin = {
+        "KrohnkiteMonocleLayout" = [];
+        "Overview" = "Meta+A";
+        "Switch to Desktop 1" = "Meta+1";
+        "Switch to Desktop 2" = "Meta+2";
+        "Switch to Desktop 3" = "Meta+3";
+        "Switch to Desktop 4" = "Meta+4";
+        "Switch to Desktop 5" = "Meta+5";
+        "Window Close" = "Meta+Q";
+        "Window Fullscreen" = "Alt+Return";
+        # "Window Move Center" = "Ctrl+Alt+C";
+        "Window to Desktop 1" = "Meta+!";
+        "Window to Desktop 2" = "Meta+@";
+        "Window to Desktop 3" = "Meta+#";
+        "Window to Desktop 4" = "Meta+$";
+        "Window to Desktop 5" = "Meta+%";
+      };
+
+      plasmashell = {
+        "show-on-mouse-pos" = "";
+      };
+
+      "services/org.kde.dolphin.desktop"."_launch" = "Meta+F";
+    };
+
+    spectacle = {
+      shortcuts = {
+        captureEntireDesktop = "";
+        captureRectangularRegion = "";
+        launch = "";
+        recordRegion = "Meta+Shift+R";
+        recordScreen = "Meta+Ctrl+R";
+        recordWindow = "";
       };
     };
 
@@ -119,6 +172,107 @@
       };
     };
 
+    panels = [
+      {
+        floating = false;
+        height = 34;
+        lengthMode = "fill";
+        location = "top";
+        opacity = "translucent";
+        widgets = [
+          {
+            name = "org.dhruv8sh.kara";
+            config = {
+              general = {
+                animationDuration = 0;
+                spacing = 3;
+                type = 1;
+              };
+              type1 = {
+                fixedLen = 3;
+                labelSource = 0;
+              };
+            };
+          }
+          "org.kde.plasma.panelspacer"
+          {
+            name = "org.kde.plasma.digitalclock";
+            config = {
+              Appearance = {
+                dateDisplayFormat = "BesideTime";
+                dateFormat = "custom";
+                use24hFormat = 2;
+              };
+            };
+          }
+          "org.kde.plasma.panelspacer"
+          {
+            systemTray = {
+              items = {
+                showAll = false;
+                shown = [
+                  "org.kde.plasma.battery"
+                  "org.kde.plasma.keyboardlayout"
+                  "org.kde.plasma.networkmanagement"
+                  "org.kde.plasma.volume"
+                ];
+                hidden = [
+                  "org.kde.plasma.brightness"
+                  "org.kde.plasma.clipboard"
+                  "org.kde.plasma.devicenotifier"
+                  "plasmashell_microphone"
+                ];
+                configs = {
+                  "org.kde.plasma.notifications".config = {
+                    Shortcuts = {
+                      global = "Meta+N";
+                    };
+                  };
+                  "org.kde.plasma.clipboard".config = {
+                    Shortcuts = {
+                      global = "Meta+V";
+                    };
+                  };
+                };
+              };
+            };
+          }
+        ];
+      }
+    ];
+
+    window-rules = [
+      {
+        apply = {
+          noborder = {
+            value = true;
+            apply = "initially";
+          };
+        };
+        description = "Hide titlebar by default";
+        match = {
+          window-class = {
+            value = ".*";
+            type = "regex";
+          };
+        };
+      }
+      # {
+      #   apply = {
+      #     desktops = "Desktop_2";
+      #     desktopsrule = "3";
+      #   };
+      #   description = "Assign Kitty to Desktop 2";
+      #   match = {
+      #     window-class = {
+      #       value = "kitty";
+      #       type = "substring";
+      #     };
+      #     window-types = ["normal"];
+      #   };
+      # }
+    ];
+
     powerdevil = {
       AC = {
         autoSuspend.action = "sleep";
@@ -146,9 +300,88 @@
       };
     };
 
+    configFile = {
+      baloofilerc."Basic Settings"."Indexing-Enabled" = false;
+      kdeglobals = {
+        General = {
+          BrowserApplication = "zen.desktop";
+        };
+        KDE = {
+          AnimationDurationFactor = 0;
+        };
+      };
+      klaunchrc.FeedbackStyle.BusyCursor = false;
+      klipperrc.General.MaxClipItems = 1000;
+      kwinrc = {
+        Effect-overview.BorderActivate = 9;
+        Plugins = {
+          krohnkiteEnabled = true;
+          screenedgeEnabled = false;
+        };
+        "Round-Corners" = {
+          ActiveOutlineAlpha = 255;
+          ActiveOutlineUseCustom = false;
+          ActiveOutlineUsePalette = true;
+          ActiveSecondOutlineUseCustom = false;
+          ActiveSecondOutlineUsePalette = true;
+          DisableOutlineTile = false;
+          DisableRoundTile = false;
+          InactiveCornerRadius = 8;
+          InactiveOutlineAlpha = 0;
+          InactiveOutlineUseCustom = false;
+          InactiveOutlineUsePalette = true;
+          InactiveSecondOutlineAlpha = 0;
+          InactiveSecondOutlineThickness = 0;
+          OutlineThickness = 1;
+          SecondOutlineThickness = 0;
+          Size = 8;
+        };
+        "Script-krohnkite" = {
+          floatingClass = "ulauncher,org.kde.kcalc";
+          screenGapBetween = 3;
+          screenGapBottom = 3;
+          screenGapLeft = 3;
+          screenGapRight = 3;
+          screenGapTop = 3;
+        };
+        Windows = {
+          DelayFocusInterval = 0;
+          FocusPolicy = "FocusFollowsMouse";
+        };
+      };
+      plasmanotifyrc = {
+        DoNotDisturb.WhenScreenSharing = false;
+        Notifications.PopupTimeout = 7000;
+      };
+      plasmarc.OSD.Enabled = false;
+      spectaclerc = {
+        Annotations.annotationToolType = 8;
+        General = {
+          launchAction = "DoNotTakeScreenshot";
+          showCaptureInstructions = false;
+          showMagnifier = "ShowMagnifierAlways";
+          useReleaseToCapture = true;
+        };
+        ImageSave.imageCompressionQuality = 100;
+      };
+    };
+
     session = {
       general.askForConfirmationOnLogout = false;
       sessionRestore.restoreOpenApplicationsOnLogin = "startWithEmptySession";
+    };
+
+    dataFile = {
+      "dolphin/view_properties/global/.directory"."Dolphin"."ViewMode" = 1;
+      "dolphin/view_properties/global/.directory"."Settings"."HiddenFilesShown" = true;
+    };
+
+    startup.startupScript = {
+      ulauncher = {
+        text = "ulauncher --hide-window";
+        priority = 8;
+        runAlways = true;
+      };
     };
   };
 }
