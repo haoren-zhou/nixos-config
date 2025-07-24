18437d857c452143ce51ead132907d3948f62342
Hao Ren Zhou <104418647+haoren-zhou@users.noreply.github.com>
2025-07-24 11:53:10 +0800

    add disk.nix

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -3,15 +3,16 @@
     ./audio.nix
     ./bluetooth.nix
     ./boot.nix
+    ./disk.nix
     ./env.nix
     ./home-manager.nix
+    ./hyprland.nix
     ./kernel.nix
     ./networking.nix
     ./nh.nix
     ./nix.nix
+    ./thunar.nix
     ./timezone_locale.nix
     ./user.nix
-    ./hyprland.nix
-    ./thunar.nix
   ];
 }
