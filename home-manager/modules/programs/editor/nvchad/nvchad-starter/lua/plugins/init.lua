146fd8b37b707d79d638daeb7a587e53690ce317
Hao Ren Zhou <104418647+haoren-zhou@users.noreply.github.com>
2025-07-30 22:43:59 +0800

    update treesitter langs

--- home-manager/modules/programs/editor/nvchad/nvchad-starter/lua/plugins/init.lua
+++ home-manager/modules/programs/editor/nvchad/nvchad-starter/lua/plugins/init.lua
@@ -16,13 +16,32 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "dockerfile",
+        "gitignore",
+        "html",
+        "javascript",
+        "json",
+        "htmldjango",
+        "lua",
+        "nix",
+        "python",
+        "requirements",
+        "scss",
+        "sql",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+    },
+  },
 }
