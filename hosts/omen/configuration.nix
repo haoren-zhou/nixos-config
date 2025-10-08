812bf4f0cd37f20bb6f9de26c081b87aa334b06f
Hao Ren Zhou <104418647+haoren-zhou@users.noreply.github.com>
2025-10-08 23:32:53 +0100

    enable openssh

--- hosts/omen/configuration.nix
+++ hosts/omen/configuration.nix
@@ -28,7 +28,18 @@
   };
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [39182];
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      # UseDns = true;
+      # X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+  networking.firewall.allowedTCPPorts = [39182];
 
   system.stateVersion = stateVersion;
 }
