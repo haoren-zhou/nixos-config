e25f3f8c5d8eaae0026b3227255b684f89e3d126
Hao Ren Zhou <104418647+haoren-zhou@users.noreply.github.com>
2026-05-01 14:12:29 +0100

    migrate nvim-treesitter to nvim 0.12

--- home-manager/modules/programs/editor/nvchad/default.nix
+++ home-manager/modules/programs/editor/nvchad/default.nix
@@ -11,40 +11,42 @@
   programs.nvchad = {
     enable = true;
     neovim = pkgs-unstable.neovim;
-    extraPackages = with pkgs; [
-      # Language Servers
-      docker-compose-language-service
-      dockerfile-language-server-nodejs
-      vscode-langservers-extracted
-      typescript-language-server
-      tailwindcss-language-server
-      texlab
-      nixd
-      basedpyright
-      marksman
-      cmake-language-server
-      ocamlPackages.ocaml-lsp
-      jdt-language-server
+    extraPackages = with pkgs;
+      [
+        # Language Servers
+        docker-compose-language-service
+        dockerfile-language-server-nodejs
+        vscode-langservers-extracted
+        typescript-language-server
+        tailwindcss-language-server
+        texlab
+        nixd
+        basedpyright
+        marksman
+        cmake-language-server
+        ocamlPackages.ocaml-lsp
+        jdt-language-server
 
-      # Formatters
-      alejandra
-      cmake-format
-      google-java-format
-      nodePackages.prettier
-      prettierd
-      ruff
-      tex-fmt
-      stylua
-      ocamlPackages.ocamlformat
+        # Formatters
+        alejandra
+        cmake-format
+        google-java-format
+        nodePackages.prettier
+        prettierd
+        ruff
+        tex-fmt
+        stylua
+        ocamlPackages.ocamlformat
 
-      # misc
-      clang-tools
-      ghostscriptX
-      imagemagick
-      librsvg
-      # python313Packages.pylatexenc
-      # gdb
-    ];
+        # misc
+        clang-tools
+        ghostscriptX
+        imagemagick
+        librsvg
+        # python313Packages.pylatexenc
+        # gdb
+      ]
+      ++ [pkgs-unstable.tree-sitter];
     hm-activation = true;
     backup = false;
   };
