ded914ea09f6b1f812351d3532eacf97fffdef70
haoren-zhou <hrz9009@gmail.com>
2026-07-21 15:30:21 +0800

    fix: update package names

--- modules/desktop/thunar.nix
+++ modules/desktop/thunar.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.thunar = {
     enable = true;
-    plugins = with pkgs.xfce; [
+    plugins = with pkgs; [
       thunar-archive-plugin # Archive management
       thunar-volman # Volume management (automount removable devices)
       thunar-media-tags-plugin # Tagging & renaming feature for media files
