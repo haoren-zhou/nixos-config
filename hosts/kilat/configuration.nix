6c6ca3085c96840a348e98473b49f5cab48223c3
haoren-zhou <hrz9009@gmail.com>
2026-07-21 16:45:20 +0800

    feat(boot): switch to limine secure boot

--- hosts/kilat/configuration.nix
+++ hosts/kilat/configuration.nix
@@ -13,4 +13,11 @@
   nixpkgs.config.allowUnfree = true;
 
   system.stateVersion = stateVersion;
+
+  # windows dual-boot
+  boot.loader.limine.extraEntries = ''
+    /Windows
+      protocol: efi_chainload
+      image_path: guid(80fbf49e-dd1f-419d-9c42-5ef8f46daf5f):/EFI/Microsoft/Boot/bootmgfw.efi
+  '';
 }
