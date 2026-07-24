8307facdcbd99deedf566caf90a2aedbf03b5a0a
Hao Ren Zhou <104418647+haoren-zhou@users.noreply.github.com>
2026-07-24 11:37:31 +0800

    feat(omen): add boot config for omen
    add windows dual-boot config and hack for hp firmware

--- hosts/omen/configuration.nix
+++ hosts/omen/configuration.nix
@@ -12,6 +12,45 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  # HACK: HP firmware quirk
+  nixpkgs.overlays = [
+    (final: prev: {
+      efibootmgr = final.symlinkJoin {
+        name = "efibootmgr-wrapped-${prev.efibootmgr.version}";
+        paths = [
+          (final.writeShellScriptBin "efibootmgr" ''
+            valid_vars=$(${prev.efibootmgr}/bin/efibootmgr)
+            new_args=()
+            modify_next=0
+            for arg in "$@"; do
+              if [ "$modify_next" = "1" ]; then
+                IFS=',' read -ra ORDER <<< "$arg"
+                valid_order=()
+                declare -A seen
+                for entry in "''${ORDER[@]}"; do
+                  if [[ "$valid_vars" == *"Boot$entry"* ]]; then
+                    if [ -z "''${seen[$entry]:-}" ]; then
+                      valid_order+=("$entry")
+                      seen[$entry]=1
+                    fi
+                  fi
+                done
+                arg=$(IFS=,; echo "''${valid_order[*]}")
+                modify_next=0
+              fi
+              if [ "$arg" = "-o" ]; then
+                modify_next=1
+              fi
+              new_args+=("$arg")
+            done
+            exec ${prev.efibootmgr}/bin/efibootmgr "''${new_args[@]}"
+          '')
+          prev.efibootmgr
+        ];
+      };
+    })
+  ];
+
   services.openssh = {
     enable = true;
     ports = [39182];
@@ -22,6 +61,13 @@
     };
   };
   networking.firewall.allowedTCPPorts = [39182];
+
+  # windows dual-boot
+  boot.loader.limine.extraEntries = ''
+    /Windows
+      protocol: efi_chainload
+      image_path: guid(c9db70df-ce04-40c5-861e-392ea2b1cb75):/EFI/Microsoft/Boot/bootmgfw.efi
+  '';
 
   system.stateVersion = stateVersion;
 }
