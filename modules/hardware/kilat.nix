dcaae2623a6dadbf043d250fb04feb9be3a85dc3
haoren-zhou <hrz9009@gmail.com>
2026-07-28 23:30:25 +0800

    feat(kilat): add ddcci-driver config for kilat for external monitor brightness control

--- modules/hardware/kilat.nix
+++ modules/hardware/kilat.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  user,
   config,
   pkgs,
   lib,
@@ -13,4 +14,19 @@
   ];
 
   hardware.nvidia.open = true;
+
+  hardware.i2c.enable = true;
+  users.users.${user}.extraGroups = ["i2c"];
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [ddcci-driver];
+  boot.kernelModules = ["ddcci-backlight"];
+
+  # from: https://wiki.nixos.org/wiki/Backlight
+  services.udev.extraRules = let
+    bash = "${pkgs.bash}/bin/bash";
+    ddcciDev = "NVIDIA i2c adapter 7 at 8:00.0";
+    ddcciNode = "/sys/bus/i2c/devices/i2c-6/new_device";
+  in ''
+    SUBSYSTEM=="i2c", ACTION=="add", ATTR{name}=="${ddcciDev}", RUN+="${bash} -c 'sleep 30; printf ddcci\ 0x37 > ${ddcciNode}'"
+  '';
 }
